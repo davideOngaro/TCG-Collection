c84b1d38b970b758e178c530bfff2f31b52b5a00
Davide <davide.ongaro.9700@gmail.com>
2025-06-07 10:38:08 +0200

    project setup

---
+++ src/main/java/com/personal/tcgcollector/TcgcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.personal.tcgcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcgcollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/tcgcollector/TcgcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.tcgcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
