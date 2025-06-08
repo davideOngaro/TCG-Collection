227d0a4117ec850edb0cb9b9b4157fb79b25dfce
Davide <davide.ongaro.9700@gmail.com>
2025-06-08 08:43:30 +0200

    import Feign

--- src/main/java/com/personal/tcgcollector/TcgcollectorApplication.java
+++ src/main/java/com/personal/tcgcollector/TcgcollectorApplication.java
@@ -2,8 +2,10 @@ package com.personal.tcgcollector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TcgcollectorApplication {
 
 	public static void main(String[] args) {
